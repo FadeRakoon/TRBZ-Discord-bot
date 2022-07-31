1712719cb11c612dd5fb320a87070d665b2f8d61
FadeRakoon <110274955+FadeRakoon@users.noreply.github.com>
2022-07-30 21:45:19 -0500

    Copying python script from repl.it to new file

---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+import discord
+from keep_alive import keep_alive
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print("I'm in")
+    print(client.user)
+
+
+@client.event
+async def on_message(message):
+    if message.author != client.user:
+        if message.content.startswith('$test'):
+            channel = message.channel
+            await channel.send('copy')
+
+
+keep_alive()
+token = os.environ.get("DISCORD_BOT_SECRET")
+client.run(token)
+token = os.environ.get('DISCORD_BOT_SECRET')
+client.run(token)
+
+#checks that message author isnt the same as the client (the bot) so that the bot doesnt respond to its own messages
+
+#@client.event
+#async def on_message(message):
+#    if message.author != client.user:
+#        await message.channel.send(message.content[::-1])
+
+#takes $greet as trigger, tells user ot say hello
+# makes function called check, checks message content for "hello" and that the channel matches the initial channel the $greet was triggered in then replies with "hello + author of user that said hello"
+
+#@client.event
+#async def on_message(message):
+#    if message.content.startswith('$greet'):
+#        channel = message.channel
+#        await channel.send('Say hello!')
+#
+#        def check(m):
+#            return m.content == 'hello' and m.channel == channel
+#
+#        msg = await client.wait_for('message', check=check)
+#        await channel.send('Hello {.author}!'.format(msg))
