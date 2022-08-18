69399010d253e039227ee8901837525642426ec0
FadeRakoon <110274955+FadeRakoon@users.noreply.github.com>
2022-08-17 22:20:51 -0500

    testing

--- main.py
+++ main.py
@@ -30,7 +30,17 @@ async def on_message(message):
         msg = await client.wait_for('message', check=check)
         await channel.send('Hello {.author}!'.format(msg))
 
+@client.event
+async def on_message(message):
+    if message.content.startswith('$greet'):
+        channel = message.channel
+        await channel.send('Say hello!')
 
+        def check(m):
+            return m.content == 'hello' and m.channel == channel
+
+        msg = await client.wait_for('message', check=check)
+        await channel.send('Hello {.author}!'.format(msg))
 
 
 
